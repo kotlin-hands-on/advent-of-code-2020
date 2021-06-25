76af74ed85e2598641b9e7f7c4567f25ff7da29c
Pasha Finkelshteyn <pavel.finkelshtein@gmail.com>
2021-06-25 20:47:30 +0300

    Update report.kt

--- src/day1/report.kt
+++ src/day1/report.kt
@@ -16,4 +16,33 @@ fun main() {
             }
         }
     }
+
+    /* FP solution */
+
+    numbers.asSequence()
+        .flatMapIndexed { idx, first ->
+            numbers.subList(idx + 1, numbers.size).asSequence().flatMapIndexed { idx2, second ->
+                numbers.subList(idx + idx2 + 2, numbers.size).asSequence().map { third ->
+                    Triple(first, second, third)
+                }
+            }
+        }
+        .filter { it.first + it.second + it.third == 2020L }
+        .onEach { println(it.first * it.second * it.third) }
+        .first()
+    
+    /* with generators */
+
+    sequence { 
+        for (first in numbers.indices)
+            for (second in (first+1) until numbers.size)
+                for (third in (second+1) until numbers.size)
+                    yield(Triple(numbers[first], numbers[second], numbers[third]))
+    }
+        .filter { it.first + it.second + it.third == 2020L }
+        .map { it.first * it.second * it.third }
+        .onEach { println(it) }
+        .first()
+
 }
+

6e73aaa8510d61959e77170d16922b6d7e4e17f3
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2021-06-28 22:06:16 +0200

    Improved solution of the 1st day simplified

--- src/day1/improvedPerformance.kt
+++ src/day1/improvedPerformance.kt
@@ -5,21 +5,18 @@ import java.io.File
 fun List<Int>.findPairOfSum(sum: Int): Pair<Int, Int>? {
     // Map: sum - x -> x
     val complements = associateBy { sum - it }
-    return mapNotNull { number ->
+    return firstNotNullOfOrNull { number ->
         val complement = complements[number]
         if (complement != null) Pair(number, complement) else null
-    }.firstOrNull()
+    }
 }
 
-fun List<Int>.findTripleOfSum(): Triple<Int, Int, Int>? {
-    // Map: x -> (y, z) where y + z = 2020 - x
-    val complementPairs: Map<Int, Pair<Int, Int>?> =
-        associateWith { findPairOfSum(2020 - it) }
-    val (x, pair) = complementPairs.entries
-        .firstOrNull { (_, pair) -> pair != null }
-        ?: return null
-    return Triple(x, pair!!.first, pair.second)
-}
+fun List<Int>.findTripleOfSum(): Triple<Int, Int, Int>? =
+    firstNotNullOfOrNull { x ->
+        findPairOfSum(2020 - x)?.let { pair ->
+            Triple(x, pair.first, pair.second)
+        }
+    }
 
 fun main() {
     val numbers = File("src/day1/input.txt")
@@ -31,5 +28,5 @@ fun main() {
 
     val triple = numbers.findTripleOfSum()
     println(triple)
-    println(triple?.let { (x, y, z) -> x * y * z} )
+    println(triple?.let { (x, y, z) -> x * y * z })
 }

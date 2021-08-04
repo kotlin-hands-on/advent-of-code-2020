e00b0ae7d63136d684a42f7627c9debfd19be0f7
anton <ant.arhipov@gmail.com>
2021-08-04 14:28:54 +0300

    solution for Day 6

---
+++ src/day06/day6.kt
@@ -0,0 +1,18 @@
+package day06
+
+import java.io.File
+
+val newLine = System.lineSeparator()
+
+fun main() {
+
+    val answer = File("src/day06/input.txt")
+        .readText()
+        .trim()
+        .split("$newLine$newLine").sumOf {
+            it.replace(newLine, "").toList().distinct().size
+        }
+
+    println(answer)
+
+}

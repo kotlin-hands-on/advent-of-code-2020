21246cc38681df16f9c22785757ed6e858937b7c
anton <ant.arhipov@gmail.com>
2021-08-05 11:36:31 +0300

    Day 6 ready

--- src/day06/day6.kt
+++ src/day06/day6.kt
@@ -2,17 +2,24 @@ package day06
 
 import java.io.File
 
-val newLine = System.lineSeparator()
 
 fun main() {
 
-    val answer = File("src/day06/input.txt")
+    val newLine = System.lineSeparator()
+
+    val groups: List<String> = File("src/day06/input.txt")
         .readText()
         .trim()
-        .split("$newLine$newLine").sumOf {
-            it.replace(newLine, "").toSet().size
-        }
+        .split("$newLine$newLine")
 
-    println(answer)
+    val firstAnswer = groups.sumOf {
+        it.replace(newLine, "").toSet().size
+    }
 
+    println("First answer: $firstAnswer")
+
+    val secondAnswer = groups.map { it.split(newLine).map { s -> s.toSet() } }
+        .fold(0) { sum, group -> sum + group.fold(group.first()) { a, b -> a intersect b }.count() }
+
+    println("Second answer: $secondAnswer")
 }

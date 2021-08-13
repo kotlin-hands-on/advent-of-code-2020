fd80d09160adfbce581a2b845c2c23fe7cc1da9a
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2021-08-13 19:08:16 +0200

    Added solution for day 5

---
+++ src/day05/day5.kt
@@ -0,0 +1,23 @@
+package day05
+
+import java.io.File
+
+fun main() {
+    val seatIDs = File("src/day05/input.txt")
+        .readLines()
+        .map(String::toSeatID)
+    val maxID = seatIDs.maxOrNull()!!
+    println("Max seat ID: $maxID")
+
+    val occupiedSeatsSet = seatIDs.toSet()
+    fun isOccupied(seat: Int) = seat in occupiedSeatsSet
+    val mySeat = (1..maxID).find { index ->
+        !isOccupied(index) && isOccupied(index - 1) && isOccupied(index + 1)
+    }
+    println("My seat ID: $mySeat")
+}
+
+fun String.toSeatID(): Int =
+    replace("B", "1").replace("F", "0")
+        .replace("R", "1").replace("L", "0")
+        .toInt(radix = 2)

4fc51a14c04969f8a95d6fe319da8f073e6c1835
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2021-09-09 15:01:46 +0200

    Provide alternative solution for day8 that includes additional interesting language concepts (sealed classes, lambdas, lazy sequences

---
+++ src/day08/day8_1.kt
@@ -0,0 +1,60 @@
+package day08.one
+
+import java.io.File
+
+val instructions = File("src/day08/input.txt")
+    .readLines()
+    .map(Instruction::fromString)
+
+fun main() {
+    println(execute(instructions))
+    println(mutate(instructions).map { execute(it) }.first { it.ip >= instructions.size })
+}
+
+data class MachineState(val ip: Int, val acc: Int)
+
+fun execute(instructions: List<Instruction>): MachineState {
+    var state = MachineState(0, 0)
+    val encounteredIndices = mutableSetOf<Int>()
+    while (state.ip < instructions.size) {
+        val nextInstruction = instructions[state.ip]
+        state = nextInstruction.action(state)
+        if (state.ip in encounteredIndices) return state
+        encounteredIndices += state.ip
+    }
+    println("No loop found – program terminates!")
+    return state
+}
+
+fun mutate(instructions: List<Instruction>) = sequence<List<Instruction>> {
+    for ((index, instruction) in instructions.withIndex()) {
+        val newProgram = instructions.toMutableList()
+        newProgram[index] = when (instruction) {
+            is Jmp -> Nop(instruction.value)
+            is Nop -> Jmp(instruction.value)
+            is Acc -> continue
+        }
+        yield(newProgram)
+    }
+}
+
+sealed class Instruction(val action: (MachineState) -> MachineState) {
+    companion object {
+        fun fromString(s: String): Instruction {
+            val (instr, immediate) = s.split(" ")
+            val value = immediate.toInt()
+            return when (instr) {
+                "nop" -> Nop(value)
+                "acc" -> Acc(value)
+                "jmp" -> Jmp(value)
+                else -> error("Invalid opcode!")
+            }
+        }
+    }
+}
+
+class Nop(val value: Int) : Instruction({ MachineState(it.ip + 1, it.acc) })
+
+class Acc(val value: Int) : Instruction({ MachineState(it.ip + 1, it.acc + value) })
+
+class Jmp(val value: Int) : Instruction({ MachineState(it.ip + value, it.acc) })

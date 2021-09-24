6469ea3e5e4240fa88006e3f9cb48ae53bf32be8
Pasha Finkelshteyn <pavel.finkelshtein@gmail.com>
2021-09-24 13:38:02 +0300

    Update day7_2.kt

--- src/day07/day7_2.kt
+++ src/day07/day7_2.kt
@@ -2,42 +2,39 @@ package day07
 
 import java.io.File
 
-val containedInBag = hashMapOf<String, Collection<String>>()
-
 private const val SHINY_GOLD = "shiny gold"
 
 fun main() {
-    buildBagTree()
-    println(SHINY_GOLD.childrenCount)
-}
-
-private fun buildBagTree() {
-    File("src/day07/input.txt")
-        .forEachLine { raw ->
-            val parentWithChildren = raw
-                .replace(Regex("bags?\\.?"), "")
-                .split("contain")
-            val parent = parentWithChildren[0].trim()
-            val children =
-                if (!raw.contains("no other"))
-                    parentWithChildren[1].split(',').map { it.trim() }
-                else setOf()
-            containedInBag[parent] = children
-        }
+    val rules = buildBagTree()
+    val result = rules.getChildrenCountBFS(SHINY_GOLD)
+    println(result)
 }
 
 val digits = "\\d+".toRegex()
 
-private val String.childrenCount: Int
-    get() {
-        val children = containedInBag.getOrDefault(this, setOf())
-        if (children.isEmpty()) return 0
-        var total = 0
-        for (child in children) {
-            val count = digits.findAll(child).first().value.toInt()
-            val bag = digits.replace(child, "").trim()
-            total += count + count * bag.childrenCount
-        }
-        return total
+private fun Map<Color, Rule>.getChildrenCountBFS(color: Color): Int {
+    val children = getOrDefault(color, setOf())
+    if (children.isEmpty()) return 0
+    var total = 0
+    for (child in children) {
+        val count = digits.findAll(child).first().value.toInt()
+        val bag = digits.replace(child, "").trim()
+        total += count + count * getChildrenCountBFS(bag)
     }
+    return total
+}
 
+private fun buildBagTree(): Map<Color, Rule> {
+    val rules = hashMapOf<Color, Rule>()
+    File("src/day07/input.txt")
+        .forEachLine { line ->
+            val (parent, allChildren) = line.replace(Regex("bags?\\.?"), "")
+                .split("contain")
+                .map { it.trim() }
+            val rule =
+                if (allChildren.contains("no other")) emptySet()
+                else allChildren.split(',').map { it.trim() }.toSet()
+            rules[parent] = rule
+        }
+    return rules
+}
